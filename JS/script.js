ee0829f0ef47c78e538994a74ea56cf931a2f224
EA - Rafael Gomes Do Nascimento <nascimento_rafael@educadventista.org>
2025-05-14 19:49:15 -0300

    Inicio do JavaScript

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const form  = documentElementById("")
+    const tabela = documentElementById("")
+
+    let cadastros = []
+    let cadastrosEditando = null
+})
